34b803318c48a1aded07e8cd87032e9ed3a1cecf
Egor <egorstrah95@gmail.com>
2025-05-09 22:37:12 -0700

    Better structure

--- Client/myGame/MyGame.java
+++ Client/myGame/MyGame.java
@@ -1,7 +1,11 @@
 package myGame;
 
 import org.joml.Math;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 import tage.*;
+import tage.audio.*;
 import tage.input.*;
 import tage.input.action.*;
 import tage.networking.IGameConnection.ProtocolType;
@@ -9,29 +13,36 @@ import tage.physics.*;
 import tage.shapes.*;
 
 import net.java.games.input.Component;
-import org.joml.*;
 
-import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import tage.audio.*;
 
 /**
  * Main game class. Handles initialization, updates, networking, and input.
  */
 public class MyGame extends VariableFrameRateGame {
+	private static final float AVATAR_SCALE = 0.1f;
+	private static final float TORUS_SCALE = 0.25f;
+	private static final float TERRAIN_Y_OFFSET = -9f;
+	private static final float TERRAIN_SCALE = 60f;
+	private static final float GROUND_Y_OFFSET = -4f;
+	private static final float CEILING_Y_OFFSET = 4f;
+	private static final float PLANE_SCALE = 20f;
+
 	private static Engine engine;
 	private InputManager inputManager;
 	private GhostManager ghostManager;
 	private ProtocolClient protocolClient;
 
+	private Ball ball;
+	private NPC npc;
 	private PhysicsEngine physicsEngine;
 
 	private GameObject avatar;
 	private TextureImage paddleTexture;
-	private ObjShape ghostShape, paddleShape;
-	private AnimatedShape paddleS;
+	private ObjShape ghostShape;
+	private AnimatedShape paddleS, paddleS_2;
 
 	private boolean isLeftSide = true;
 	private boolean isClientConnected = false;
@@ -42,15 +53,11 @@ public class MyGame extends VariableFrameRateGame {
 	private final String serverAddress;
 	private final int serverPort;
 	private final ProtocolType serverProtocol;
-
 	private final float[] matrixValues = new float[16];
 
 	private double startTime;
-
 	private Matrix4f avatarOriginalRotation;
-
 	private Sound bounceSound;
-
 	private PongNPCandBall pong;
 
 	public MyGame(String serverAddress, int serverPort, String protocol) {
@@ -71,9 +78,10 @@ public class MyGame extends VariableFrameRateGame {
 	@Override
 	public void loadShapes() {
 		ghostShape = new ImportedModel("paddle.obj");
-//		paddleShape = new ImportedModel("paddle.obj");
 		paddleS = new AnimatedShape("pong.rkm", "pong.rks");
 		paddleS.loadAnimation("Bounce", "bounce.rka");
+		paddleS_2 = new AnimatedShape("pong.rkm", "pong.rks");
+		paddleS_2.loadAnimation("Bounce", "bounce.rka");
 	}
 
 	@Override
@@ -93,70 +101,71 @@ public class MyGame extends VariableFrameRateGame {
 
 	private void buildAvatar() {
 		avatar = new GameObject(GameObject.root(), paddleS, paddleTexture);
-		avatar.setLocalScale(new Matrix4f().scaling(0.25f));
+		avatar.setLocalScale(new Matrix4f().scaling(AVATAR_SCALE));
 		setLockedX(-5f);
 		setLockedZ(-3f);
-		Matrix4f initialRot = new Matrix4f()
-				.rotateY((float) Math.toRadians(90))
-				.rotateX((float) Math.toRadians(90));
-		avatar.setLocalRotation(initialRot);
 
-		Matrix4f avatarTranslation = new Matrix4f(avatar.getLocalTranslation());
-		double[] avatarTransform = toDoubleArray(avatarTranslation.get(matrixValues));
-		PhysicsObject avatarPhysics = engine.getSceneGraph().addPhysicsBox(1.0f, avatarTransform, new float[]{0.5f, 2.0f, 0.5f});
+		avatar.setLocalRotation(new Matrix4f()
+				.rotateY(Math.toRadians(90))
+				.rotateX(Math.toRadians(90)));
+
+		PhysicsObject avatarPhysics = engine.getSceneGraph().addPhysicsBox(
+				1.0f,
+				toDoubleArray(avatar.getLocalTranslation().get(matrixValues)),
+				new float[]{0.5f, 2.0f, 0.5f}
+		);
 		avatar.setPhysicsObject(avatarPhysics);
 	}
 
 	private void buildTorus() {
 		GameObject torus = new GameObject(GameObject.root(), new Torus(0.5f, 0.2f, 48));
 		torus.setLocalTranslation(new Matrix4f().translation(1f, 0f, 0f));
-		torus.setLocalScale(new Matrix4f().scaling(0.25f));
+		torus.setLocalScale(new Matrix4f().scaling(TORUS_SCALE));
 	}
 
 	private void buildAxes() {
-		GameObject xAxis = new GameObject(GameObject.root(), new Line(new Vector3f(0, 0, 0), new Vector3f(3, 0, 0)));
-		GameObject yAxis = new GameObject(GameObject.root(), new Line(new Vector3f(0, 0, 0), new Vector3f(0, 3, 0)));
-		GameObject zAxis = new GameObject(GameObject.root(), new Line(new Vector3f(0, 0, 0), new Vector3f(0, 0, -3)));
+		createAxis(new Vector3f(3, 0, 0), new Vector3f(1, 0, 0)); // X - Red
+		createAxis(new Vector3f(0, 3, 0), new Vector3f(0, 1, 0)); // Y - Green
+		createAxis(new Vector3f(0, 0, -3), new Vector3f(0, 0, 1)); // Z - Blue
+	}
 
-		xAxis.getRenderStates().setColor(new Vector3f(1, 0, 0)); // Red
-		yAxis.getRenderStates().setColor(new Vector3f(0, 1, 0)); // Green
-		zAxis.getRenderStates().setColor(new Vector3f(0, 0, 1)); // Blue
+	private void createAxis(Vector3f end, Vector3f color) {
+		GameObject axis = new GameObject(GameObject.root(), new Line(new Vector3f(0, 0, 0), end));
+		axis.getRenderStates().setColor(color);
 	}
 
 	private void buildTerrain() {
 		GameObject terrain = new GameObject(GameObject.root(), new TerrainPlane(512), new TextureImage("rocks.jpg"));
-		terrain.setLocalTranslation(new Matrix4f().translation(0f, -9f, 0f));
-		terrain.setLocalScale(new Matrix4f().scaling(60f, 6f, 60f));
+		terrain.setLocalTranslation(new Matrix4f().translation(0f, TERRAIN_Y_OFFSET, 0f));
+		terrain.setLocalScale(new Matrix4f().scaling(TERRAIN_SCALE, 6f, TERRAIN_SCALE));
 		terrain.setHeightMap(new TextureImage("hills.jpg"));
-
 		terrain.getRenderStates().setColor(new Vector3f(0.5f, 0f, 0f));
 		terrain.getRenderStates().setTiling(1);
 		terrain.getRenderStates().setTileFactor(10);
 	}
 
 	private void buildGroundPlane() {
-		GameObject groundPlane = new GameObject(GameObject.root(), new Plane());
-		groundPlane.setLocalTranslation(new Matrix4f().translation(0f, -4f, 0f));
-		groundPlane.setLocalScale(new Matrix4f().scaling(20f));
-
-		double[] groundTransform = toDoubleArray(new Matrix4f(groundPlane.getLocalTranslation()).get(matrixValues));
-		PhysicsObject groundPhysics = engine.getSceneGraph().addPhysicsStaticPlane(groundTransform, new float[]{0f, 1f, 0f}, 0f);
-		groundPhysics.setBounciness(0.8f);
-
-		groundPlane.setPhysicsObject(groundPhysics);
-		groundPlane.getRenderStates().disableRendering(); // Make it invisible
+		addPhysicsPlane(GROUND_Y_OFFSET, new float[]{0f, 1f, 0f}, true);
 	}
 
 	private void buildCeilingPlane() {
-		GameObject ceilingPlane = new GameObject(GameObject.root(), new Plane());
-		ceilingPlane.setLocalTranslation(new Matrix4f().translation(0f, 4f, 0f));
-		ceilingPlane.setLocalScale(new Matrix4f().scaling(20f));
+		addPhysicsPlane(CEILING_Y_OFFSET, new float[]{0f, -1f, 0f}, false);
+	}
 
-		double[] ceilingTransform = toDoubleArray(new Matrix4f(ceilingPlane.getLocalTranslation()).get(matrixValues));
-		PhysicsObject ceilingPhysics = engine.getSceneGraph().addPhysicsStaticPlane(ceilingTransform, new float[]{0f, -1f, 0f}, 0f);
-		ceilingPhysics.setBounciness(0.8f);
+	private void addPhysicsPlane(float yOffset, float[] normal, boolean invisible) {
+		GameObject plane = new GameObject(GameObject.root(), new Plane());
+		plane.setLocalTranslation(new Matrix4f().translation(0f, yOffset, 0f));
+		plane.setLocalScale(new Matrix4f().scaling(PLANE_SCALE));
 
-		ceilingPlane.setPhysicsObject(ceilingPhysics);
+		PhysicsObject physics = engine.getSceneGraph().addPhysicsStaticPlane(
+				toDoubleArray(plane.getLocalTranslation().get(matrixValues)),
+				normal,
+				0f
+		);
+		physics.setBounciness(0.8f);
+		plane.setPhysicsObject(physics);
+
+		if (invisible) plane.getRenderStates().disableRendering();
 	}
 
 	@Override
@@ -179,7 +188,9 @@ public class MyGame extends VariableFrameRateGame {
 		startTime = System.currentTimeMillis();
 		engine.getRenderSystem().setWindowDimensions(1900, 1000);
 		engine.enablePhysicsWorldRender();
-		pong = new PongNPCandBall(this);
+
+		ball = new Ball(this);
+		npc = new NPC(this);
 
 		physicsEngine = engine.getSceneGraph().getPhysicsEngine();
 		physicsEngine.setGravity(new float[]{0f, -9.8f, 0f});
@@ -195,7 +206,6 @@ public class MyGame extends VariableFrameRateGame {
 		AudioResource bounceResource = audioMgr.createAudioResource("bounce.wav", AudioResourceType.AUDIO_SAMPLE);
 		bounceSound = new Sound(bounceResource, SoundType.SOUND_EFFECT, 25, false);
 		bounceSound.initialize(audioMgr);
-
 	}
 
 	private void setupNetworking() {
@@ -223,19 +233,6 @@ public class MyGame extends VariableFrameRateGame {
 		inputManager.associateActionWithAllGamepads(Component.Identifier.Axis.X, new TurnAction(this), InputManager.INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
 	}
 
-	private class TestAction extends AbstractInputAction {
-		@Override
-		public void performAction(float time, net.java.games.input.Event event) {
-			paddleS.stopAnimation();
-			paddleS.playAnimation("Bounce", 0.25f, AnimatedShape.EndType.PAUSE, 0);
-			if (bounceSound != null) {
-				float randomPitch = 0.9f + (float)(java.lang.Math.random()); // random between 0.9 and 1.1
-				bounceSound.setPitch(randomPitch);
-				bounceSound.play();
-			}
-		}
-	}
-
 	private void setupCamera() {
 		Camera camera = engine.getRenderSystem().getViewport("MAIN").getCamera();
 		camera.setLocation(new Vector3f(0f, 1f, 6f));
@@ -243,88 +240,80 @@ public class MyGame extends VariableFrameRateGame {
 		camera.setV(new Vector3f(0, 1, 0));
 		camera.setN(new Vector3f(0, 0, -1));
 
-		Matrix4f pitchDown = new Matrix4f().rotation((float)Math.toRadians(-5), new Vector3f(1, 0, 0));
+		Matrix4f pitchDown = new Matrix4f().rotation((float) Math.toRadians(-5), new Vector3f(1, 0, 0));
+		camera.setU(transformVector(camera.getU(), pitchDown));
+		camera.setV(transformVector(camera.getV(), pitchDown));
+		camera.setN(transformVector(camera.getN(), pitchDown));
+	}
 
-		Vector3f u = camera.getU();
-		Vector3f v = camera.getV();
-		Vector3f n = camera.getN();
-
-		Vector4f newU = new Vector4f(u, 0).mul(pitchDown);
-		Vector4f newV = new Vector4f(v, 0).mul(pitchDown);
-		Vector4f newN = new Vector4f(n, 0).mul(pitchDown);
-
-		camera.setU(new Vector3f(newU.x, newU.y, newU.z));
-		camera.setV(new Vector3f(newV.x, newV.y, newV.z));
-		camera.setN(new Vector3f(newN.x, newN.y, newN.z));
+	private Vector3f transformVector(Vector3f vector, Matrix4f matrix) {
+		Vector4f result = new Vector4f(vector, 0).mul(matrix);
+		return new Vector3f(result.x, result.y, result.z);
 	}
 
 	@Override
 	public void update() {
-		double elapsedTime = System.currentTimeMillis() - startTime;
-		startTime = System.currentTimeMillis();
+		double currentTime = System.currentTimeMillis();
+		double elapsedTime = currentTime - startTime;
+		startTime = currentTime;
 
 		updateHUD();
-
 		inputManager.update((float) elapsedTime);
 		if (protocolClient != null) protocolClient.processPackets();
 		if (physicsEngine != null) physicsEngine.update((float) elapsedTime);
 
-		if (avatar != null && avatar.getPhysicsObject() != null) {
-			syncAvatarPhysics();
-		}
+		if (avatar != null && avatar.getPhysicsObject() != null) syncAvatarPhysics();
+		if (protocolClient != null) protocolClient.sendMoveMessage(avatar.getWorldLocation());
 
-		if (protocolClient != null) {
-			protocolClient.sendMoveMessage(avatar.getWorldLocation());
-		}
+		ghostManager.getGhostAvatars().forEach(GhostAvatar::syncToPhysics);
 
-		for (GhostAvatar ghost : ghostManager.getGhostAvatars()) {
-			ghost.syncToPhysics();
-		}
-
-		if (pong != null) {
-			pong.update((float)elapsedTime);
+		if (ball != null && npc != null) {
+			ball.update((float) elapsedTime, npc.getNPCPaddle(), getPlayerPosition());
+			npc.update((float) elapsedTime, ball.getBall());
 		}
 
 		paddleS.updateAnimation();
+		paddleS_2.updateAnimation();
 	}
 
 	private void updateHUD() {
 		Camera camera = engine.getRenderSystem().getViewport("MAIN").getCamera();
-
 		String hud1 = "Time = " + (int) ((System.currentTimeMillis() - startTime) / 1000);
 		String hud2 = String.format("Camera position = %.2f, %.2f, %.2f",
-				camera.getLocation().x(),
-				camera.getLocation().y(),
-				camera.getLocation().z());
+				camera.getLocation().x(), camera.getLocation().y(), camera.getLocation().z());
 
 		engine.getHUDmanager().setHUD1(hud1, new Vector3f(1, 0, 0), 15, 15);
 		engine.getHUDmanager().setHUD2(hud2, new Vector3f(1, 1, 1), 500, 15);
 	}
 
 	private void syncAvatarPhysics() {
-		double[] transform = avatar.getPhysicsObject().getTransform();
+		PhysicsObject physics = avatar.getPhysicsObject();
+		double[] transform = physics.getTransform();
 		transform[12] = lockedX;
 		transform[14] = lockedZ;
-		avatar.getPhysicsObject().setTransform(transform);
+		physics.setTransform(transform);
 
-		float[] velocity = avatar.getPhysicsObject().getLinearVelocity();
+		float[] velocity = physics.getLinearVelocity();
 		velocity[0] = velocity[2] = 0f;
-		avatar.getPhysicsObject().setLinearVelocity(velocity);
+		physics.setLinearVelocity(velocity);
 
-		float[] angularVelocity = avatar.getPhysicsObject().getAngularVelocity();
+		float[] angularVelocity = physics.getAngularVelocity();
 		angularVelocity[0] = angularVelocity[1] = angularVelocity[2] = 0f;
-		avatar.getPhysicsObject().setAngularVelocity(angularVelocity);
+		physics.setAngularVelocity(angularVelocity);
 
-		Matrix4f fullMatrix = new Matrix4f().set(toFloatArray(avatar.getPhysicsObject().getTransform()));
+		Matrix4f matrix = new Matrix4f().set(toFloatArray(physics.getTransform()));
 		Vector3f translation = new Vector3f();
-		fullMatrix.getTranslation(translation);
-
+		matrix.getTranslation(translation);
 		avatar.setLocalTranslation(new Matrix4f().translation(translation));
 	}
 
+	// ------------------------
+	// Public API Methods
+	// ------------------------
 
 	public GameObject getAvatar() { return avatar; }
 	public AnimatedShape getPaddleS() { return paddleS; }
+	public AnimatedShape getPaddleS_2() { return paddleS_2; }
 	public Camera getCamera() { return engine.getRenderSystem().getViewport("MAIN").getCamera(); }
 	public ObjShape getGhostShape() { return ghostShape; }
 	public TextureImage getGhostTexture() { return paddleTexture; }
@@ -332,46 +321,71 @@ public class MyGame extends VariableFrameRateGame {
 	public Engine getEngine() { return engine; }
 	public float getLockedX() { return lockedX; }
 	public float getLockedZ() { return lockedZ; }
+
 	public void setLockedX(float x) { lockedX = x; }
-	public void setLockedZ(float z) { lockedZ = z;	}
-	public void setAvatarOriginalRotation(Matrix4f rotation) { this.avatarOriginalRotation = new Matrix4f(rotation); }
-	public Matrix4f getAvatarOriginalRotation() { return new Matrix4f(avatarOriginalRotation); }
+	public void setLockedZ(float z) { lockedZ = z; }
 
+	public void setAvatarOriginalRotation(Matrix4f rotation) {
+		avatarOriginalRotation = new Matrix4f(rotation);
+	}
 
-	public Vector3f getPlayerPosition() { return avatar.getWorldLocation(); }
+	public Matrix4f getAvatarOriginalRotation() {
+		return new Matrix4f(avatarOriginalRotation);
+	}
+
+	public Vector3f getPlayerPosition() {
+		return avatar.getWorldLocation();
+	}
+
 	public void setIsConnected(boolean connected) {
 		isClientConnected = connected;
-		if (pong != null) {
-			pong.setIsNPCActive(!connected);
-		}
+		if (npc != null) npc.setIsNPCActive(!connected);
 	}
+
+	public Sound getBounceSound() { return bounceSound; }
 	public void setLeftSide(boolean side) { isLeftSide = side; }
 	public boolean getLeftSide() { return isLeftSide; }
+	public Ball getBall() { return ball; }
+
+	// ------------------------
+	// Private Helper Classes
+	// ------------------------
 
 	private class SendCloseConnectionPacketAction extends AbstractInputAction {
 		@Override
 		public void performAction(float time, net.java.games.input.Event event) {
-			if (protocolClient != null && isClientConnected) {
-				protocolClient.sendByeMessage();
+			if (protocolClient != null && isClientConnected) protocolClient.sendByeMessage();
+		}
+	}
+
+	private class TestAction extends AbstractInputAction {
+		@Override
+		public void performAction(float time, net.java.games.input.Event event) {
+			paddleS.stopAnimation();
+			paddleS.playAnimation("Bounce", 0.25f, AnimatedShape.EndType.PAUSE, 0);
+			if (bounceSound != null) {
+				float randomPitch = 0.9f + (float) Math.random() * 0.2f; // between 0.9 and 1.1
+				bounceSound.setPitch(randomPitch);
+				bounceSound.play();
 			}
 		}
 	}
 
+	// ------------------------
+	// Private Utility Methods
+	// ------------------------
+
 	private double[] toDoubleArray(float[] array) {
 		if (array == null) return null;
 		double[] result = new double[array.length];
-		for (int i = 0; i < array.length; i++) {
-			result[i] = array[i];
-		}
+		for (int i = 0; i < array.length; i++) result[i] = array[i];
 		return result;
 	}
 
 	private float[] toFloatArray(double[] array) {
 		if (array == null) return null;
 		float[] result = new float[array.length];
-		for (int i = 0; i < array.length; i++) {
-			result[i] = (float) array[i];
-		}
+		for (int i = 0; i < array.length; i++) result[i] = (float) array[i];
 		return result;
 	}
 }
